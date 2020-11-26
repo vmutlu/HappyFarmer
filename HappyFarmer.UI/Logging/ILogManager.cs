cc1ba8c4a07019529de99b2677b1603a02ad4806
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-11-26 17:51:41 +0300

    Projede Log sınıflarının gereksiz using blokları temizlendi.

--- HappyFarmer.UI/Logging/ILogManager.cs
+++ HappyFarmer.UI/Logging/ILogManager.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace HappyFarmer.UI.Logging
+﻿namespace HappyFarmer.UI.Logging
 {
     public interface ILogManager<T> where T : class
     {
