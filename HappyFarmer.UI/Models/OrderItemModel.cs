752a8a8e203fc92adcbdc0d43db717ba0cdf5a3d
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-11-27 17:01:29 +0300

    OrderItemModel Sınıfı gereksiz using blokları temizlendi.

--- HappyFarmer.UI/Models/OrderItemModel.cs
+++ HappyFarmer.UI/Models/OrderItemModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace HappyFarmer.UI.Models
+﻿namespace HappyFarmer.UI.Models
 {
     public class OrderItemModel
     {
