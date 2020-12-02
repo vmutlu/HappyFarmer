41c68be51684656801a5f617ef3effb7b6f92874
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-12-02 15:36:15 +0300

    Data Access Katmanında yer alan interface sınıflarının gereksiz using alanları temizlendi.

--- HappyFarmer.DataAccess/Abstract/IBlogRepository.cs
+++ HappyFarmer.DataAccess/Abstract/IBlogRepository.cs
@@ -1,7 +1,4 @@
 ﻿using HappyFarmer.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace HappyFarmer.DataAccess.Abstract
 {
--- HappyFarmer.DataAccess/Abstract/IMessageRepository.cs
+++ HappyFarmer.DataAccess/Abstract/IMessageRepository.cs
@@ -1,7 +1,5 @@
 ﻿using HappyFarmer.Entities;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace HappyFarmer.DataAccess.Abstract
 {
