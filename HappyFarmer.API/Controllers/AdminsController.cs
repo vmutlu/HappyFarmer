cd9be8c0f15c7fdf206b3aaf32f8f6cbf5fe665f
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-12-08 21:29:03 +0300

    API Katmanına Admin Controller isminde controller sınıfı eklendi. Örnek olması için Product sınıfının Get metodu yazıldı.

---
+++ HappyFarmer.API/Controllers/AdminsController.cs
@@ -0,0 +1,24 @@
+﻿using HappyFarmer.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HappyFarmer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminsController : ControllerBase
+    {
+
+        private readonly IProductService _productService;
+        public AdminsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = _productService.GetAll();
+            return Ok(products);
+        }
+    }
+}
