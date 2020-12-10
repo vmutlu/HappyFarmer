e0fed27043afac72a4a5e43d8f384c0a941cda08
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-12-10 17:20:49 +0300

    Admin Panelinde ki GetAllProducts metodu düzenlendi.

--- HappyFarmer.API/Controllers/AdminsController.cs
+++ HappyFarmer.API/Controllers/AdminsController.cs
@@ -1,5 +1,7 @@
-﻿using HappyFarmer.Business.Abstract;
+﻿using HappyFarmer.API.DTOs;
+using HappyFarmer.Business.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace HappyFarmer.API.Controllers
 {
@@ -18,7 +20,37 @@ namespace HappyFarmer.API.Controllers
         public IActionResult GetAllProducts()
         {
             var products = _productService.GetAll();
-            return Ok(products);
+
+            var productDTO = products != null ?
+                (from i in products
+                 select new FarmerProductDTO
+                 {
+                     AnimalAge = i.AnimalAge,
+                     AnnouncementDate = i.AnnouncementDate,
+                     City = i.City,
+                     Country = i.Country,
+                     Description = i.Country,
+                     FarmerDeclareType = i.FarmerDeclareType,
+                     Gender = i.Gender,
+                     Guarantee = i.Guarantee,
+                     Id = i.Id,
+                     ImageUrl = i.ImageUrl,
+                     Name = i.Name,
+                     Neighborhood = i.Neighborhood,
+                     PermissionToSell = i.PermissionToSell,
+                     Price = i.Price,
+                     Situation = i.Situation,
+                     StockQty = i.StockQty,
+                     Swap = i.Swap,
+                     Title = i.Title,
+                     UserEMail = i.UserEMail,
+                     UserId = i.UserId,
+                     UserName = i.UserName,
+                     UserPhoneNumber = i.UserPhoneNumber,
+                     UserSurname = i.UserSurname
+                 }).ToList() : null;
+
+            return Ok(productDTO);
         }
     }
 }
