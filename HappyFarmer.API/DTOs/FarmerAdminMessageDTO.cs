83ee9ff05441fa5dccce68a59dc7f936fdd6ea52
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-12-15 17:28:25 +0300

    API Katmanındaki DTO klasörüne iki DTO sınıfı eklendi.

---
+++ HappyFarmer.API/DTOs/FarmerAdminMessageDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace HappyFarmer.API.DTOs
+{
+    public class FarmerAdminMessageDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public bool Seen { get; set; } = false;
+    }
+}
---
+++ HappyFarmer.API/DTOs/FarmerBannerDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HappyFarmer.API.DTOs
+{
+    public class FarmerBannerDTO
+    {
+        public int Id { get; set; }
+        public string ImageURL { get; set; }
+        public bool Active { get; set; }
+        public int QueueNumber { get; set; }
+        public bool? LowerUpper { get; set; }
+    }
+}
