39f4469d919d99f69d1eb3e9b00d02a2224e55a9
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-12-15 15:49:32 +0300

    FarmerProductCommentDTO sınıfı DTO klasörü içerisine eklendi.

---
+++ HappyFarmer.API/DTOs/FarmerProductCommentDTO.cs
@@ -0,0 +1,19 @@
+﻿using HappyFarmer.Entities;
+
+namespace HappyFarmer.API.DTOs
+{
+    public class FarmerProductCommentDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public FarmerUser User { get; set; }
+        public string Comment { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string WebSite { get; set; }
+        public string CommentDate { get; set; }
+        public int ProductId { get; set; }
+        public FarmerProduct Product { get; set; }
+    }
+}
