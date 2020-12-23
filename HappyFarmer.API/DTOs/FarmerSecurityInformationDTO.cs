4f3b37aeaf199c13cd992d4aa145d3a996caf1df
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-12-23 20:50:50 +0300

    SecurityInformationDTO sınıfı eklendi.

---
+++ HappyFarmer.API/DTOs/FarmerSecurityInformationDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HappyFarmer.API.DTOs
+{
+    public class FarmerSecurityInformationDTO
+    {
+        public int Id { get; set; }
+        public string AdversitingRule { get; set; }
+        public string TermsOfUse { get; set; }
+        public string PrivacyPolicy { get; set; }
+        public string FAQ { get; set; }
+    }
+}
