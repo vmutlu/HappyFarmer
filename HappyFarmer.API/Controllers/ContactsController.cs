3ead26a7b279cef0a35687ca95e9bfa4825028c8
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-12-24 17:02:52 +0300

    ContactController sınıfı API Katmanına eklendi. Endpointler yazıldı.

---
+++ HappyFarmer.API/Controllers/ContactsController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HappyFarmer.API.DTOs;
+using HappyFarmer.Business.Abstract;
+using HappyFarmer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HappyFarmer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IAdminMessageService _adminMessageService;
+        private readonly IMapper _mapper;
+        public ContactsController(IAdminMessageService adminMessageService, IMapper mapper)
+        {
+            _adminMessageService = adminMessageService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult SendAdminMessage(FarmerAdminMessageDTO farmerAdminMessageDTO)
+        {
+            _adminMessageService.Create(_mapper.Map<FarmerAdminMessage>(farmerAdminMessageDTO));
+
+            return Ok();
+        }
+    }
+}
