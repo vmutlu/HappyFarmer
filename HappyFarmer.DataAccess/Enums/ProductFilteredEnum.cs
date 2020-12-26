b8f6ede3b53f641c918666da45b618e2d7ffa539
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-12-26 22:21:52 +0300

    Product Filter için Enum sınıfı eklendi.

---
+++ HappyFarmer.DataAccess/Enums/ProductFilteredEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace HappyFarmer.DataAccess.Enums
+{
+    public enum ProductFilteredEnum
+    {
+        A_Z_ye = 0,
+        Z_A_ya = 1,
+        Son_ilk = 2,
+        Ilk_son = 3
+    }
+}
