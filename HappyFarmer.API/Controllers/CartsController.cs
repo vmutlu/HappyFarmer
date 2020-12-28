cb37792cea1279d27adf9a258c53bdcf082c7b76
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-12-28 16:51:28 +0300

    API Katmanına Carts isimli yeni Controller sınıfı eklendi.

---
+++ HappyFarmer.API/Controllers/CartsController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using HappyFarmer.API.DTOs;
+using HappyFarmer.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HappyFarmer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartsController : ControllerBase
+    {
+        private readonly ICartService _cartService;
+        private readonly IOrderService _orderService;
+        private readonly IProductService _productService;
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+        public CartsController(ICartService cartService, IOrderService orderService, IProductService productService, IUserService userService, IMapper mapper)
+        {
+            _cartService = cartService;
+            _orderService = orderService;
+            _productService = productService;
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("ActiveUser")]
+        public IActionResult GetCartByUserId(string activeCustomerId)
+        {
+            var cart = _cartService.GetCartByUserId(activeCustomerId);
+
+            return Ok(_mapper.Map<FarmerCartDTO>(cart));
+        }
+
+
+    }
+}
