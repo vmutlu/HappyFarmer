fcecf08002b74279a010dc495e0b071b4f56b47c
veysel mutlu <veysel_mutlu42@hotmail.com>
2020-12-28 16:51:45 +0300

    DTO sınıfına FarmerCartDTO sınıfı eklendi.

---
+++ HappyFarmer.API/DTOs/FarmerCartDTO.cs
@@ -0,0 +1,13 @@
+﻿using HappyFarmer.Entities;
+using System.Collections.Generic;
+
+namespace HappyFarmer.API.DTOs
+{
+    public class FarmerCartDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public FarmerUser User { get; set; }
+        public List<FarmerCartItem> CartItems { get; set; }
+    }
+}
