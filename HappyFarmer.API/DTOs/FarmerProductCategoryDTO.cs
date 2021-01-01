fd679913229711aefb87f3e6cffc7fcdaf50367a
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-01-01 21:56:56 +0300

    API Katmanı DTO sınıfılarının gereksiz using blokları temizlendi.

--- HappyFarmer.API/DTOs/FarmerProductCategoryDTO.cs
+++ HappyFarmer.API/DTOs/FarmerProductCategoryDTO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace HappyFarmer.API.DTOs
+﻿namespace HappyFarmer.API.DTOs
 {
     public class FarmerProductCategoryDTO
     {
