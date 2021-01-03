1b3c990ced453e5c995dc916d5b171a4319faa87
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-01-03 21:45:01 +0300

    UI Katmanı startup.cs sınıfındaki yorum satıları silindi

--- HappyFarmer.UI/Startup.cs
+++ HappyFarmer.UI/Startup.cs
@@ -19,7 +19,6 @@ namespace HappyFarmer.UI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<ILog, LogNLog>();
@@ -27,7 +26,6 @@ namespace HappyFarmer.UI
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILog logger)
         {
             if (env.IsDevelopment())
@@ -38,7 +36,6 @@ namespace HappyFarmer.UI
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.ConfigureExceptionHandler(logger);
