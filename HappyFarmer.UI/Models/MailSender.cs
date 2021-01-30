dfeb009d777026eb5ee62ba687f017505e1381bd
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-01-30 20:24:13 +0300

    .

--- HappyFarmer.UI/Models/MailSender.cs
+++ HappyFarmer.UI/Models/MailSender.cs
@@ -28,7 +28,7 @@ namespace HappyFarmer.UI.Models
                 EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(from.Address, "Mail Adrese girilen email adresin şifresi girilecek")
+                Credentials = new NetworkCredential(from.Address, "")
             })
             {
                 using (var message = new MailMessage(from, response) { Subject = submit, Body = body })
