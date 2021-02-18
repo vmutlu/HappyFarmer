7a9c3ee30906af1590b985cb4d3d279217ad1e54
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-02-18 10:48:50 +0300

    UI kısmında startup yapılanmasında register edilen repo ve serviceler business katmanına eklenen autofac kütüphanesi yapılandırması yapıldı.

--- HappyFarmer.UI/Program.cs
+++ HappyFarmer.UI/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using HappyFarmer.Business.DependencyResolvers.Autofac;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace HappyFarmer.UI
 {
@@ -18,6 +15,11 @@ namespace HappyFarmer.UI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            .ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterModule(new AutofacBusinessModule());
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
