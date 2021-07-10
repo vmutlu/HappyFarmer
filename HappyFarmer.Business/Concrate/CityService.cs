f9b2a0f14c1adb92d8e0d91a1eea72391b324ad9
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-07-10 13:06:40 +0300

    CityService clases code cleanup

--- HappyFarmer.Business/Concrate/CityService.cs
+++ HappyFarmer.Business/Concrate/CityService.cs
@@ -8,18 +8,13 @@ namespace HappyFarmer.Business.Concrate
     public class CityService : ICityService
     {
         private readonly ICityRepository _cityRepository;
-        public CityService(ICityRepository cityRepository)
-        {
+        public CityService(ICityRepository cityRepository) =>
             _cityRepository = cityRepository;
-        }
-        public List<FarmerCities> GetAll()
-        {
-            return _cityRepository.GetAll();
-        }
 
-        public FarmerCities GetById(int id)
-        {
-            return _cityRepository.GetById(id);
-        }
+        public List<FarmerCities> GetAll() =>
+             _cityRepository.GetAll();
+
+        public FarmerCities GetById(int id) =>
+             _cityRepository.GetById(id);
     }
 }
