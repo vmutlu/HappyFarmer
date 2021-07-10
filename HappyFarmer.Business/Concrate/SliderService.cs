150e0b4d8dfd82cdf0ed37dbaef3df7787908da9
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-07-10 13:08:19 +0300

    SliderService clases code cleanup

--- HappyFarmer.Business/Concrate/SliderService.cs
+++ HappyFarmer.Business/Concrate/SliderService.cs
@@ -8,33 +8,21 @@ namespace HappyFarmer.Business.Concrate
     public class SliderService : ISliderService
     {
         private ISliderRepository _sliderRepository;
-        public SliderService(ISliderRepository sliderRepository)
-        {
+        public SliderService(ISliderRepository sliderRepository) =>
             _sliderRepository = sliderRepository;
-        }
-        public void Create(FarmerSlider entity)
-        {
+        public void Create(FarmerSlider entity) =>
             _sliderRepository.Create(entity);
-        }
 
-        public void Delete(FarmerSlider entity)
-        {
+        public void Delete(FarmerSlider entity) =>
             _sliderRepository.Delete(entity);
-        }
 
-        public List<FarmerSlider> GetAll(bool? authority = false)
-        {
-            return _sliderRepository.GetAll(null, authority);
-        }
+        public List<FarmerSlider> GetAll(bool? authority = false) =>
+             _sliderRepository.GetAll(null, authority);
 
-        public FarmerSlider GetById(int id)
-        {
-            return _sliderRepository.GetById(id);
-        }
+        public FarmerSlider GetById(int id) =>
+             _sliderRepository.GetById(id);
 
-        public void Update(FarmerSlider entity)
-        {
+        public void Update(FarmerSlider entity) =>
             _sliderRepository.Update(entity);
-        }
     }
 }
