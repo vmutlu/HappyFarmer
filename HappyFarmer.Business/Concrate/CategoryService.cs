8a11646f8ba5786056ff16567f1d772e91fb012f
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-07-10 13:06:31 +0300

    CategoryService clases code cleanup

--- HappyFarmer.Business/Concrate/CategoryService.cs
+++ HappyFarmer.Business/Concrate/CategoryService.cs
@@ -10,45 +10,30 @@ namespace HappyFarmer.Business.Concrate
     public class CategoryService : ICategoryService
     {
         private readonly ICategoryRepository _categoryRepository;
-        public CategoryService(ICategoryRepository categoryRepository)
-        {
+        public CategoryService(ICategoryRepository categoryRepository) =>
             _categoryRepository = categoryRepository;
-        }
+
 
         [ValidationAspect(typeof(CategoryValidator))]
-        public void Create(FarmerCategory entity)
-        {
+        public void Create(FarmerCategory entity) =>
             _categoryRepository.Create(entity);
-        }
 
-        public void Delete(FarmerCategory entity)
-        {
+        public void Delete(FarmerCategory entity) =>
             _categoryRepository.Delete(entity);
-        }
 
-        public void DeleteFromCategory(int categoryId, int productId)
-        {
+        public void DeleteFromCategory(int categoryId, int productId) =>
             _categoryRepository.DeleteFromCategory(categoryId, productId);
-        }
 
-        public List<FarmerCategory> GetAll()
-        {
-            return _categoryRepository.GetAll();
-        }
+        public List<FarmerCategory> GetAll() =>
+             _categoryRepository.GetAll();
 
-        public FarmerCategory GetById(int id)
-        {
-            return _categoryRepository.GetById(id);
-        }
+        public FarmerCategory GetById(int id) =>
+             _categoryRepository.GetById(id);
 
-        public FarmerCategory GetByIdWithProducts(int id)
-        {
-            return _categoryRepository.GetByIdWithProduct(id);
-        }
+        public FarmerCategory GetByIdWithProducts(int id) =>
+             _categoryRepository.GetByIdWithProduct(id);
 
-        public void Update(FarmerCategory entity)
-        {
+        public void Update(FarmerCategory entity) =>
             _categoryRepository.Update(entity);
-        }
     }
 }
