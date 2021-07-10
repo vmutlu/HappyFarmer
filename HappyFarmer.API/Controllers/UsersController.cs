9fb91fe46615669346b03e7688dfdf62a8210bd3
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-07-10 17:10:37 +0300

    UsersController classes code cleanup applied

--- HappyFarmer.API/Controllers/UsersController.cs
+++ HappyFarmer.API/Controllers/UsersController.cs
@@ -1,7 +1,6 @@
 ﻿using HappyFarmer.Business.Abstract;
 using HappyFarmer.Entities;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 
 namespace HappyFarmer.API.Controllers
 {
@@ -10,53 +9,44 @@ namespace HappyFarmer.API.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IUserService _userService;
-        public UsersController(IUserService userService)
-        {
+        public UsersController(IUserService userService) =>
             _userService = userService;
-        }
 
         [HttpGet]
-        public async Task<IActionResult>GetAll()
-        {
-            var response = _userService.GetAll();
-            return Ok(response);
-        }
+        public IActionResult GetAll() => Ok(_userService.GetAll());
 
         [HttpGet("{id}")]
-        public async Task<IActionResult>GetById(int id)
-        {
-            var response = _userService.GetById(id);
-            return Ok(response);
-        }
+        public IActionResult GetById(int id) => Ok(_userService.GetById(id));
 
         [HttpPost]
-        public async Task<IActionResult> Add(FarmerUser user)
+        public IActionResult Add(FarmerUser user)
         {
             _userService.Create(user);
             return Ok();
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update(FarmerUser user)
+        public IActionResult Update(FarmerUser user)
         {
             _userService.Update(user);
             return Ok();
         }
 
         [HttpDelete]
-        public async Task<IActionResult> Delete(FarmerUser user)
+        public IActionResult Delete(FarmerUser user)
         {
             _userService.Delete(user);
             return Ok();
         }
 
         [HttpPost("PasswordSignIn")]
-        public async Task<IActionResult> PasswordSignIn(FarmerUser user)
+        public IActionResult PasswordSignIn(FarmerUser user)
         {
             var userEmail = user.Email;
             var userPassword = user.Password;
             var userType = user.UserType;
             var response = _userService.PasswordSignIn(userEmail, userPassword, userType);
+
             if (!response)
                 return NotFound();
             else
