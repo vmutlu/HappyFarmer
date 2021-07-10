7cea46fe3f7869d420fcd4cba02fb647eb19334d
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-07-10 13:08:27 +0300

    UserService clases code cleanup

--- HappyFarmer.Business/Concrate/UserService.cs
+++ HappyFarmer.Business/Concrate/UserService.cs
@@ -8,63 +8,40 @@ namespace HappyFarmer.Business.Concrate
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
-        public UserService(IUserRepository userRepository)
-        {
+        public UserService(IUserRepository userRepository) =>
             _userRepository = userRepository;
-        }
-        public void Create(FarmerUser entity)
-        {
+
+        public void Create(FarmerUser entity) =>
             _userRepository.Create(entity);
-        }
 
-        public void Delete(FarmerUser entity)
-        {
+        public void Delete(FarmerUser entity) =>
             _userRepository.Delete(entity);
-        }
 
-        public List<FarmerUser> FindByEmail(string email)
-        {
-            return _userRepository.FindByEmail(email);
-        }
+        public List<FarmerUser> FindByEmail(string email) =>
+             _userRepository.FindByEmail(email);
 
-        public List<FarmerUser> GetAll()
-        {
-            return _userRepository.GetAll();
-        }
+        public List<FarmerUser> GetAll() =>
+             _userRepository.GetAll();
 
-        public List<FarmerUser> GetAllCarrier()
-        {
-            return _userRepository.GetAllCarrier();
-        }
+        public List<FarmerUser> GetAllCarrier() =>
+             _userRepository.GetAllCarrier();
 
-        public List<FarmerUser> GetAllCustomer()
-        {
-            return _userRepository.GetAllCustomer();
-        }
+        public List<FarmerUser> GetAllCustomer() =>
+             _userRepository.GetAllCustomer();
 
-        public List<FarmerUser> GetAllOnlyCustomer()
-        {
-            return _userRepository.GetAllOnlyCustomer();
-        }
+        public List<FarmerUser> GetAllOnlyCustomer() =>
+             _userRepository.GetAllOnlyCustomer();
 
-        public FarmerUser GetById(int id)
-        {
-            return _userRepository.GetById(id);
-        }
+        public FarmerUser GetById(int id) =>
+             _userRepository.GetById(id);
 
-        public List<FarmerOrderItem> GetUserSoldProduct(int userId)
-        {
-            return _userRepository.GetUserSoldProduct(userId);
-        }
+        public List<FarmerOrderItem> GetUserSoldProduct(int userId) =>
+             _userRepository.GetUserSoldProduct(userId);
 
-        public bool PasswordSignIn(string userEmail, string userPassword, int userType)
-        {
-            return _userRepository.PasswordSignIn(userEmail, userPassword,userType);
-        }
+        public bool PasswordSignIn(string userEmail, string userPassword, int userType) =>
+             _userRepository.PasswordSignIn(userEmail, userPassword, userType);
 
-        public void Update(FarmerUser entity)
-        {
+        public void Update(FarmerUser entity) =>
             _userRepository.Update(entity);
-        }
     }
 }
